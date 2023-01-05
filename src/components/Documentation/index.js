6457dce47906aa371211db8898c7708018b489e1
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-05 14:46:00 -0300

    Added Documentation

---
+++ src/components/Documentation/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Titles from '../Titles'
+import Video from '../Video'
+
+const Documentation = () => {
+  return (
+    <section className='ca-documentation section' id='ca-documentation'>
+      <div className='container-fluid'>
+        <Titles subtitle='Our Documentation' title="See what our profile is like and how we work for your business" />
+
+        <Video embedId="22M9koNqAfA" />
+      </div>
+    </section>
+  )
+}
+
+export default Documentation
